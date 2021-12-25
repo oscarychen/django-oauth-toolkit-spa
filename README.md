ddccd90d73f66443dc3a68daa4710967625ba372
Oscar Chen <oscar.y.chen@gmail.com>
2021-12-25 00:49:17 -0700

    Added tests

--- README.md
+++ README.md
@@ -1,22 +1,26 @@
 # Django-oauth-toolkit-cookie-refresh
 
-Django-oauth-toolkit-cookie-refresh is a Django app to that provides REST authentication endpoints which uses refresh token in httpOnly cookie. It relies on [Django REST framework](https://github.com/encode/django-rest-framework) and [Django Oauth Toolkit](https://github.com/jazzband/django-oauth-toolkit).
+Django-oauth-toolkit-cookie-refresh is a Django app to that provides REST authentication endpoints which uses refresh
+token in httpOnly cookie. It relies on [Django REST framework](https://github.com/encode/django-rest-framework)
+and [Django Oauth Toolkit](https://github.com/jazzband/django-oauth-toolkit).
 
 #### Motivation
 
-I was using django-oauth-toolkit in a project, but I wanted the refresh tooken to be handled by a HttpOnly cookie, while having the access token to be continue sent via request/response body.
+I was using django-oauth-toolkit in a project, but I wanted the refresh tooken to be handled by a HttpOnly cookie, while
+having the access token to be continue sent via request/response body.
 
 ## Quick start
 
 Install from source:
 
-```
+```commandline
 pip install git+https://github.com/oscarychen/django-oauth-toolkit-cookie-refresh.git
 ```
 
-Set up [django-oauth-toolkit and django REST framework](https://django-oauth-toolkit.readthedocs.io/en/latest/rest-framework/getting_started.html#step-1-minimal-setup):
+Set
+up [django-oauth-toolkit and django REST framework](https://django-oauth-toolkit.readthedocs.io/en/latest/rest-framework/getting_started.html#step-1-minimal-setup):
 
-```
+```python
 INSTALLED_APPS = (
     'django.contrib.admin',
     ...
@@ -25,7 +29,7 @@ INSTALLED_APPS = (
 )
 ```
 
-```
+```python
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
@@ -35,14 +39,14 @@ REST_FRAMEWORK = {
 
 Include the oauth_toolkit_cookie_refresh URLconf in your project urls.py:
 
-```
+```python
     path('auth/', include('oauth_toolkit_cookie_refresh.urls')),
 ```
 
 ## Settings
 
-django-oauth-toolkit's settings are largely extended and used, except few default values have been overwritten.
-These settings are used as default unless explicitly specified:
+django-oauth-toolkit's settings are largely extended and used, except few default values have been overwritten. These
+settings are used as default unless explicitly specified:
 
 ```
 "ACCESS_TOKEN_EXPIRE_SECONDS": 300,
@@ -53,44 +57,52 @@ These settings are used as default unless explicitly specified:
 
 You can modify these settings by specifying them in the settings for django-oauth-toolkit:
 
-```
+```python
 OAUTH2_PROVIDER = {
     ...
-    "ACCESS_TOKEN_EXPIRE_SECONDS": 300,
-    "REFRESH_TOKEN_EXPIRE_SECONDS": 36000,
-    "REFRESH_COOKIE_NAME": "refresh_token",
-    "REFRESH_COOKIE_PATH": "/auth",
-    ...
+"ACCESS_TOKEN_EXPIRE_SECONDS": 300,
+"REFRESH_TOKEN_EXPIRE_SECONDS": 36000,
+"REFRESH_COOKIE_NAME": "refresh_token",
+"REFRESH_COOKIE_PATH": "/auth",
+...
 }
 ```
 
-If you want to use a different path for authentication than the default path, you should provide the setting in `REFRESH_COOKIE_PATH`, using a string with leading slash `/`; while provide the same path in URLconf but with a trailing slash `/`.
+If you want to use a different path for authentication than the default path, you should provide the setting
+in `REFRESH_COOKIE_PATH`, using a string with leading slash `/`; while provide the same path in URLconf but with a
+trailing slash `/`.
 
 ## Development
 
 To continue develope this package, I've included a `requirements.txt` and `env_setup.sh` to help you get up and running.
 
-With the appropriate Python(ie: Python 3.8), run the following to set up a Python virtual environment in project directory:
+With the appropriate Python(ie: Python 3.8), run the following to set up a Python virtual environment in project
+directory:
 
-```
+```commandline
 source env_setup.py
 ```
 
-which also creates a `activate.sh`.
-and then activate the environment by running:
+which also creates a `activate.sh`. and then activate the environment by running:
 
-```
+```commandline
 source activate.sh
 ```
 
 To build this package:
 
-```
+```commandline
 python setup.py sdist
 ```
 
 To include this package symbolically in another environment:
 
-```
+```commandline
 pip install -e PATH_TO_PACKAGE_DIRECTORY
+```
+
+Automated testing
+
+```commandline
+tox
 ```
--- tests/test_auth_views.py
+++ tests/test_auth_views.py
@@ -1,66 +1,133 @@
-from rest_framework.test import APIRequestFactory
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
 from django.contrib.auth import get_user_model
 from oauth_toolkit_cookie_refresh.settings import oauth2_settings
 from oauth2_provider.models import get_application_model
-from http.cookies import SimpleCookie
+from oauth2_provider.views import ProtectedResourceView
 
 Application = get_application_model()
 UserModel = get_user_model()
 
 
-class TestAuthViews(TestCase):
+# mocking a protected resource view
+class ResourceView(ProtectedResourceView):
+    def get(self, request, *args, **kwargs):
+        return "This is a protected resource"
 
+
+class BaseTest(TestCase):
     def setUp(self) -> None:
+        self.factory = RequestFactory()
         self.user = UserModel.objects.create_user("test_user", "tester@example.com", "123456")
         self.app = Application.objects.create(
             name="test_client",
             client_type=Application.CLIENT_PUBLIC,
             authorization_grant_type=Application.GRANT_PASSWORD,
         )
-        self.refresh_token = None
         self.access_token = None
 
     def tearDown(self) -> None:
         self.user.delete()
         self.app.delete()
 
-    def test_log_in(self):
-        response = self.client.post(f"{oauth2_settings.REFRESH_COOKIE_PATH}/login/",
-                                    {
-                                        "username": "test_user",
-                                        "password": "123456",
-                                        "grant_type": "password",
-                                        "client_id": self.app.client_id
-                                    }
-                                    )
+    def _get_response_access_token(self, response):
         self.assertEqual(response.status_code, 200)
         self.assertIn("access_token", response.data)
-        access_token = response.data["access_token"]
-        self.access_token = access_token
+        # self.access_token = response.data['access_token']
+        return response.data['access_token']
+
+    def _check_response_refresh_token(self, response):
+        self.assertEqual(response.status_code, 200)
         self.assertIn(oauth2_settings.REFRESH_COOKIE_NAME, response.cookies)
         refresh_token_cookie = response.cookies[oauth2_settings.REFRESH_COOKIE_NAME]
-        self.refresh_token = refresh_token_cookie._value
-
-        # check refresh token cookie attributes
         self.assertTrue(refresh_token_cookie['httponly'])
         self.assertTrue(refresh_token_cookie['secure'])
         self.assertEqual(refresh_token_cookie['path'], oauth2_settings.REFRESH_COOKIE_PATH)
         self.assertEqual(refresh_token_cookie['max-age'], oauth2_settings.REFRESH_TOKEN_EXPIRE_SECONDS)
         self.assertEqual(refresh_token_cookie['samesite'], 'strict')
 
-    # def test_token_refresh(self):
-    #     pass
-    #     response = self.client.post(f"{oauth2_settings.REFRESH_COOKIE_PATH}/refresh/")
-    #
-    # def test_log_off(self):
-    #     pass
-    #     response = self.client.post()
-    #
-    # def test_log_off_everywhere(self):
-    #     pass
-
-    def _print(self, v):
+    @staticmethod
+    def _print(v):
         print("$$$$$$$ DEBUG $$$$$")
         print(v)
         assert False
+
+
+class TestClientResourcePasswordBased(BaseTest):
+
+    def test_client_resource_password_based(self):
+        access_token = self._log_in()
+        self._validate_access_token(access_token)
+
+        # token refresh should cause the access token previous associated with the refresh token to be removed
+        access_token_2 = self._token_refresh()
+        self._validate_access_token(access_token_2)
+        self._check_access_token_is_invalid(access_token)
+
+        self._log_off()
+        self._check_access_token_is_invalid(access_token_2)
+
+        # check log off everywhere revokes all outstanding tokens
+        access_token_3 = self._log_in()
+        access_token_4 = self._log_in()
+        self._log_off_everywhere()
+        self._check_access_token_is_invalid(access_token_3)
+        self._check_access_token_is_invalid(access_token_4)
+
+    def _log_in(self):
+        """Log in sequence."""
+        response = self.client.post(f"{oauth2_settings.REFRESH_COOKIE_PATH}/login/", {
+            "username": "test_user",
+            "password": "123456",
+            "grant_type": Application.GRANT_PASSWORD,
+            "client_id": self.app.client_id
+        })
+        access_token = self._get_response_access_token(response)
+        self._check_response_refresh_token(response)
+        return access_token
+
+    def _token_refresh(self):
+        """Obtain new access token using refresh token"""
+        response = self.client.post(f"{oauth2_settings.REFRESH_COOKIE_PATH}/refresh/", {
+            "client_id": self.app.client_id
+        })
+        access_token = self._get_response_access_token(response)
+        return access_token
+
+    def _validate_access_token(self, access_token):
+        """Use access token to access a fake resource."""
+        auth_headers = {
+            "HTTP_AUTHORIZATION": "Bearer " + access_token,
+        }
+        request = self.factory.get("/fake-resource", **auth_headers)
+        request.user = self.user
+
+        view = ResourceView.as_view()
+        response = view(request)
+        self.assertEqual(response, "This is a protected resource")
+
+    def _check_access_token_is_invalid(self, access_token):
+        """Check that access token cannot access a fake resource."""
+        auth_headers = {
+            "HTTP_AUTHORIZATION": "Bearer " + access_token,
+        }
+        request = self.factory.get("/fake-resource", **auth_headers)
+        request.user = self.user
+
+        view = ResourceView.as_view()
+        response = view(request)
+        self.assertEqual(response.status_code, 403)
+
+    def _log_off(self):
+        """Log off"""
+        self.client.post(f"{oauth2_settings.REFRESH_COOKIE_PATH}/logoff/", {
+            "client_id": self.app.client_id
+        })
+
+    def _log_off_everywhere(self):
+        """Log off everywhere by revoking all refresh token and access tokens."""
+        self.client.post(f"{oauth2_settings.REFRESH_COOKIE_PATH}/logoff-everywhere/", {
+            "username": "test_user",
+            "password": "123456",
+            "grant_type": Application.GRANT_PASSWORD,
+            "client_id": self.app.client_id
+        })
